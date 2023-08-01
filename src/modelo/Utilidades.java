7cb5e36f123ce4807e51065d9fc2c81dc55c0b2f
darwin0809 <darwin.criollo@uao.edu.co>
2023-08-01 17:09:35 -0500

    commit2

--- src/modelo/Utilidades.java
+++ src/modelo/Utilidades.java
@@ -5,10 +5,44 @@
  */
 package modelo;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.LinkedList;
+
 /**
  *
  * @author darwin.criollo
  */
 public class Utilidades {
+    public static boolean guardarArchivo(LinkedList<Cliente> listaC){
+        boolean t = false;
+        
+        try{
+            String nombreArchivo = "Clientes.txt";
+            FileWriter fileWriter = new FileWriter(nombreArchivo);
+            BufferedWriter writer = new BufferedWriter(fileWriter);
+            for (Cliente cliente : listaC){
+                writer.write(cliente.toString()+"\n");
+                
+            }
+            writer.close();
+            fileWriter.close();
+            t=true;
+            
+            
+        }
+        catch(IOException e){
+            e.printStackTrace();
+            
+        }
+
+        
+        
+        
+        
+        return t;
+        
+    }
     
 }
